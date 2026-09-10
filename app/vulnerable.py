672679f6780b4201d79118a1e4afe4f56f608e8b
Ast-work <ankitmohanty765@gmail.com>
2026-09-10 07:08:54 +0000

    Fix command injection vulnerability

--- app/vulnerable.py
+++ app/vulnerable.py
@@ -3,4 +3,12 @@ import sys
 
 user_input = sys.argv[1]
 
-subprocess.call(user_input, shell=True)
+allowed_commands = {
+    "date": ["date"],
+    "whoami": ["whoami"],
+}
+
+if user_input in allowed_commands:
+    subprocess.run(allowed_commands[user_input], check=True)
+else:
+    print("Command not allowed")
